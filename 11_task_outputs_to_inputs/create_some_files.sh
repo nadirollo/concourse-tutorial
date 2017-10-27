e4ee3d839b06f324dff647c2c94b85e026a62de6
Nadir Lloret <nadir.lloret@gmail.com>
2017-10-27 21:00:44 +0100

    Test5

--- 11_task_outputs_to_inputs/create_some_files.sh
+++ 11_task_outputs_to_inputs/create_some_files.sh
@@ -5,5 +5,6 @@ echo "file1" > some-files/file1
 echo "file2" > some-files/file2
 echo "file3" > some-files/file3
 echo "file4" > some-files/file4
-
+mkdir foo
+echo "foo1" > foo/foo1
 ls some-files/*
